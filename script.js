f67b4462580aec2aacc7fefbdebd5574bdaac227
PatrickJS <github@gdi2290.com>
2015-11-19 08:16:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
         {
           name: 'Jeff Whelpley',
           twitter: 'jeffwhelpley',
-          avatar: 'jeffwhelpley.png'
+          avatar: 'panel/jeffwhelpley.png'
         }
       ]
     ];
