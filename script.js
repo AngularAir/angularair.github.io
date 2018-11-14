ecca24541b9f9540ac9a658742af1aa2d4e31f48
Justin Schwartzenberger <jschwarty@users.noreply.github.com>
2018-11-14 08:44:07 -0800

    episodes

--- script.js
+++ script.js
@@ -47,12 +47,32 @@
 
     vm.episodes = [
       {
-        number: 183,
-        title: 'ngAir 183 - Live from AngularConnect 2018',
-        displayDate: 'Wednesday, Nov 7th, 2018',
-        date: '2018-11-07',
-        time: '12:00 AM (Pacific Time)',
-        hangoutUrl: 'https://youtu.be/quNwuCirav4',
+        number: 184,
+        title: 'ngAir 184 - The ngVikings Crew',
+        displayDate: 'Wednesday, Nov 14th, 2018',
+        date: '2018-11-14',
+        time: '11:00 AM (Pacific Time)',
+        hangoutUrl: 'https://youtu.be/rsTXgaNTbUo',
+        guests: [[]],
+        hasNotes: false,
+        notesAreVisible: false
+      },{
+        number: 185,
+        title: 'ngAir 185 - Introducing Angular for the Visual Learner With Samantha Rhodes',
+        displayDate: 'Wednesday, Nov 21st, 2018',
+        date: '2018-11-21',
+        time: '8:00 AM (Pacific Time)',
+        hangoutUrl: 'https://youtu.be/gAYJ0G514t4',
+        guests: [[]],
+        hasNotes: false,
+        notesAreVisible: false
+      },{
+        number: 186,
+        title: 'ngAir 186 - Angular Performance Anti-Patterns With Bonnie Brennan',
+        displayDate: 'Wednesday, Nov 28th, 2018',
+        date: '2018-11-28',
+        time: '11:00 AM (Pacific Time)',
+        hangoutUrl: 'https://youtu.be/bHe_XC-16G0',
         guests: [[]],
         hasNotes: false,
         notesAreVisible: false
@@ -2268,6 +2288,17 @@
         date: '2018-10-31',
         time: '11:00 AM (Pacific Time)',
         hangoutUrl: 'https://youtu.be/8PxIror_iI4',
+        guests: [[]],
+        hasNotes: false,
+        notesAreVisible: false
+      },
+      {
+        number: 183,
+        title: 'ngAir 183 - Live from AngularConnect 2018',
+        displayDate: 'Wednesday, Nov 7th, 2018',
+        date: '2018-11-07',
+        time: '12:00 AM (Pacific Time)',
+        hangoutUrl: 'https://youtu.be/quNwuCirav4',
         guests: [[]],
         hasNotes: false,
         notesAreVisible: false
